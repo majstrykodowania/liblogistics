30bad978566c5a9e83ceb2e583fe93b2112f3e5a
Lucas Dec <dec.lukasz@gmail.com>
2020-05-06 16:18:47 +0200

    add routing, create basic structure

---
+++ src/components/BooksStatus/BooksStatus.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const BooksStatus = () => <h1>BookStatus</h1>;
+
+export default BooksStatus;
---
+++ src/components/CreateForm/CreateForm.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const CreateForm = () => <h1>CreateForm</h1>;
+
+export default CreateForm;
