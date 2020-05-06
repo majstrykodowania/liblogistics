c1d1e0bc9b8c4b4d2507a51fe332cc2f631842d4
Mateusz <sznajder1@gmail.com>
2020-05-06 18:12:08 +0200

    create initial books and users database

---
+++ src/dataBase/booksDb.js
@@ -0,0 +1,122 @@
+export const booksDb = [
+  // Single Book ---------------------------------------------------
+  {
+    id: 1,
+    title: "Metro 2033",
+    author: "Jan Kowalski",
+    released: 2020,
+    total: 4,
+    descript: "wapoifj qwjfpqo jfpowqj fpowqj fowqfjqp f",
+    item: [
+      {
+        isdn: 2854,
+        events: [
+          { userId: 2, return: "06-05-2020", borrow: "01-05-2020" },
+          { userId: 3, return: "10-04-2020", borrow: "01-04-2020" },
+        ],
+      },
+      {
+        isdn: 1148,
+        events: [{ userId: 1, return: "06-05-2020", borrow: "01-05-2020" }],
+      },
+      {
+        isdn: 1132,
+        events: [{ userId: 4, return: "06-05-2020", borrow: "01-05-2020" }],
+      },
+      {
+        isdn: 2131,
+        events: [{ userId: 2, return: "06-02-2020", borrow: "01-01-2020" }],
+      },
+    ],
+  },
+  // Single Book ---------------------------------------------------
+  {
+    id: 1,
+    title: "Pociąg",
+    author: "Jan Kowalski",
+    released: 2019,
+    total: 4,
+    descript: "wapoifj qwjfpqo jfpowqj fpowqj fowqfjqp f",
+    item: [
+      {
+        isdn: 1111,
+        events: [
+          { userId: 2, return: "06-05-2020", borrow: "01-05-2020" },
+          { userId: 3, return: "10-04-2020", borrow: "01-04-2020" },
+        ],
+      },
+      {
+        isdn: 1112,
+        events: [{ userId: 1, return: "06-05-2020", borrow: "01-05-2020" }],
+      },
+      {
+        isdn: 1114,
+        events: [{ userId: 4, return: "06-05-2020", borrow: "01-05-2020" }],
+      },
+      {
+        isdn: 1115,
+        events: [{ userId: 2, return: "06-02-2020", borrow: "01-01-2020" }],
+      },
+    ],
+  },
+  // Single Book ---------------------------------------------------
+  {
+    id: 1,
+    title: "Tramwaje",
+    author: "Jan Kowalski",
+    released: 2020,
+    total: 4,
+    descript: "wapoifj qwjfpqo jfpowqj fpowqj fowqfjqp f",
+    item: [
+      {
+        isdn: 2854,
+        events: [
+          { userId: 2, return: "06-05-2020", borrow: "01-05-2020" },
+          { userId: 3, return: "10-04-2020", borrow: "01-04-2020" },
+        ],
+      },
+      {
+        isdn: 1148,
+        events: [{ userId: 1, return: "06-05-2020", borrow: "01-05-2020" }],
+      },
+      {
+        isdn: 1132,
+        events: [{ userId: 4, return: "06-05-2020", borrow: "01-05-2020" }],
+      },
+      {
+        isdn: 2131,
+        events: [{ userId: 2, return: "06-02-2020", borrow: "01-01-2020" }],
+      },
+    ],
+  },
+  // Single Book ---------------------------------------------------
+  {
+    id: 1,
+    title: "Metro 2033",
+    author: "Jan Kowalski",
+    released: 2020,
+    total: 4,
+    descript: "wapoifj qwjfpqo jfpowqj fpowqj fowqfjqp f",
+    item: [
+      {
+        isdn: 2854,
+        events: [
+          { userId: 2, return: "06-05-2020", borrow: "01-05-2020" },
+          { userId: 3, return: "10-04-2020", borrow: "01-04-2020" },
+        ],
+      },
+      {
+        isdn: 1148,
+        events: [{ userId: 1, return: "06-05-2020", borrow: "01-05-2020" }],
+      },
+      {
+        isdn: 1132,
+        events: [{ userId: 4, return: "06-05-2020", borrow: "01-05-2020" }],
+      },
+      {
+        isdn: 2131,
+        events: [{ userId: 2, return: "06-02-2020", borrow: "01-01-2020" }],
+      },
+    ],
+  },
+];
---
+++ src/dataBase/usersDb.js
@@ -0,0 +1,18 @@
+export const usersDb = [
+    {
+        id= 1,
+        books= []
+    },
+    {
+        id= 2,
+        books= [2854]
+    },
+    {
+        id= 3,
+        books= []
+    },
+    {
+        id= 4,
+        books= []
+    }
+]
