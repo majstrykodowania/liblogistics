0471c3a71aac838917fd63d62665623982aae2c5
Mateusz <sznajder1@gmail.com>
2020-05-10 19:50:25 +0200

    connected to firebase, removed old database file, added spinner while loading data

--- src/components/BooksStatus/Books/Books.js
+++ src/components/BooksStatus/Books/Books.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import SingleBook from "./SingleBook/SingleBook";
+import Spinner from "../../Spinner/Spinner";
 import classes from "./books.module.css";
 
 class Books extends Component {
+  state = {
+    loading: true,
+    books: [],
+  };
+
+  componentDidMount() {
+    // Pobranie z bazy tablicy Books i przypisane do state books
+    axios
+      .get("https://liblogistic.firebaseio.com/books.json")
+      .then((response) => {
+        this.setState({ books: response.data });
+        this.setState({ loading: false });
+      })
+      .catch((error) => console.log(error));
+  }
+
   render() {
-    return (
-      <ul className={classes.Books}>
-        <SingleBook title="Metro 2033" />
-        <SingleBook title="Harry Potter" />
-        <SingleBook title="Wiedźmin" />
-      </ul>
-    );
+    // Wyświetlenie spinnera na podstawie state loading
+    let singleBooks = <Spinner />;
+    if (!this.state.loading)
+      singleBooks = this.state.books.map((book, id) => {
+        return <SingleBook key={id} title={book.title} />;
+      });
+
+    return <ul className={classes.Books}>{singleBooks}</ul>;
   }
 }
 
